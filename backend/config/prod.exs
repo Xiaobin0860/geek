f0c9c8936451f256f326e9db1b1b062d72c71a7a
Xiaobin Li <l_xb@foxmail.com>
2018-09-02 02:14:37 +0800

    release

--- backend/config/prod.exs
+++ backend/config/prod.exs
@@ -12,6 +12,7 @@ use Mix.Config
 config :geek, GeekWeb.Endpoint,
   http: [port: System.get_env("PORT") || 4020],
   #url: [host: "localhost", port: 80],
+  server: true,
   cache_static_manifest: "priv/static/cache_manifest.json"
 
 # Do not print debug messages in production
