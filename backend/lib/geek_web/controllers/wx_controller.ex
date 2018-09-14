76a3e7fb149bb4dda5b842e7793d0faedd5dc3e0
xl000 <l_xb@foxmail.com>
2018-09-14 13:53:17 +0800

    wx

--- backend/lib/geek_web/controllers/wx_controller.ex
+++ backend/lib/geek_web/controllers/wx_controller.ex
@@ -10,7 +10,7 @@ defmodule GeekWeb.WxController do
   def login(conn, %{"code" => code}) do
     response =
       HTTPotion.get(
-        "https://api.weixin.qq.com/sns/jscode2session?appid=wx343f06195860bc6e&secret=a337c097d43a4e2b67b58123f49210ba&grant_type=authorization_code&js_code=" <>
+        "https://api.weixin.qq.com/sns/jscode2session?appid="<>Application.fetch_env!(:geek, :wx_appid)<>"&secret="<>Application.fetch_env!(:geek, :wx_secret)<>"&grant_type=authorization_code&js_code=" <>
           code
       )
 
